a8c004b6cfe39714611099caee6c411bc184c7d2
William Behn <behnwilliam@icloud.com>
2024-08-01 19:22:06 +0200

    batman

---
+++ backend/src/main/java/no/behn/guessthecommander/GuessthecommanderApplication.java
@@ -0,0 +1,13 @@
+package no.behn.guessthecommander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessthecommanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessthecommanderApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/no/behn/guessthecommander/GuessthecommanderApplicationTests.java
@@ -0,0 +1,13 @@
+package no.behn.guessthecommander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessthecommanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
